a9b0d79580ccb956c6707268a5c00079685d0226
Andreas <aw@decaf.de>
2015-03-06 03:47:44 +0100

    gulp build task

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var rename = require("gulp-rename");
 var gzip = require("gulp-gzip");
 var connect = require('gulp-connect');
 var include = require('gulp-include');
+var sizereport = require('gulp-sizereport');
 var wrap = require("gulp-wrap");
 var version = require('./package.json').version;
 
@@ -21,7 +22,7 @@ var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('scriptsTest', function () {
 
-	var browserified = transform(function(filename) {
+	var browserified = transform(function (filename) {
 		var b = browserify(filename, {
 			standalone: 'watched',
 			debug: false
@@ -41,18 +42,7 @@ gulp.task('scriptsTest', function () {
 		.pipe(gulp.dest('./test/specs'));
 });
 
-gulp.task('javascript', function () {
-	// transform regular node stream to gulp (buffered vinyl) stream
-	return gulp.src('lib/watched.js')
-		.pipe(browserified)
-		.pipe(sourcemaps.init({loadMaps: true}))
-		// Add transformation tasks to the pipeline here.
-		.pipe(uglify())
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('connect', function(){
+gulp.task('connect', function () {
 	connect.server({
 		root: ['test'],
 		port: 8000,
@@ -61,40 +51,70 @@ gulp.task('connect', function(){
 });
 
 
-
 gulp.task('uglify', function () {
-	gulp.src('lib/watched.js')
-			.pipe(include())
-			.pipe(wrap({ src: 'build/umdWrapper.tpl'}))
-			.pipe(wrap({ src: 'build/licenseHeader.tpl'}, { version: version}, { variable: 'data' }))
-			.pipe(gulp.dest('./dist'))
-			.pipe(uglify({
-				outSourceMap: false,
-				mangle: true
-			}))
-			.pipe(wrap({ src: 'build/licenseHeader.tpl'}, { version: version}, { variable: 'data' }))
-			.pipe(rename({
-				suffix: ".min"
-			}))
-			.pipe(gulp.dest('dist'))
-			.pipe(gzip())
-			.pipe(gulp.dest("dist"));
+
+	var browserified = transform(function (filename) {
+		var b = browserify(filename, {
+			standalone: 'watched',
+			debug: false
+		});
+		return b.bundle();
+	});
+
+	return gulp.src('src/watched.js')
+		.pipe(browserified)
+		.pipe(wrap({src: 'build/licenseHeader.tpl'}, {version: version}, {variable: 'data'}))
+		.pipe(gulp.dest('./dist'))
+		.pipe(uglify({
+			outSourceMap: false,
+			mangle: true
+		}))
+		.pipe(rename({
+			suffix: ".min"
+		}))
+		.pipe(gulp.dest('dist'))
+		.pipe(gzip())
+		.pipe(gulp.dest("dist"));
+
+	//gulp.src('lib/watched.js')
+	//		.pipe(include())
+	//		.pipe(wrap({ src: 'build/umdWrapper.tpl'}))
+	//		.pipe(wrap({ src: 'build/licenseHeader.tpl'}, { version: version}, { variable: 'data' }))
+	//		.pipe(gulp.dest('./dist'))
+	//		.pipe(uglify({
+	//			outSourceMap: false,
+	//			mangle: true
+	//		}))
+	//		.pipe(wrap({ src: 'build/licenseHeader.tpl'}, { version: version}, { variable: 'data' }))
+	//		.pipe(rename({
+	//			suffix: ".min"
+	//		}))
+	//		.pipe(gulp.dest('dist'))
+	//		.pipe(gzip())
+	//		.pipe(gulp.dest("dist"));
 });
 
 gulp.task("compress", function () {
 	gulp.src("dist/watched.min.js")
-			.pipe(gzip())
-			.pipe(gulp.dest("dist"));
+		.pipe(gzip())
+		.pipe(gulp.dest("dist"));
 });
 
 gulp.task("reload", function () {
 	gulp.src('lib/watched.js')
-			.pipe(connect.reload());
+		.pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
 	gulp.watch(['src/**/*'], ['scriptsTest', 'reload']);
 });
 
+gulp.task('sizereport', function () {
+	return gulp.src('./dist/*')
+		.pipe(sizereport({
+			gzip: true
+		}));
+});
+
 gulp.task('default', ['connect', 'scriptsTest', 'watch']);
-gulp.task('build', ['uglify']);
+gulp.task('build', ['uglify', 'sizereport']);
